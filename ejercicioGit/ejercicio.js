456b62603010ec4bfc036c805eead84e59ccf91a
AlejandroTene <carlostene2006@gmail.com>
2024-08-08 09:14:10 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/200w.gif"
     console.log("ingresa a saludar")
 }
