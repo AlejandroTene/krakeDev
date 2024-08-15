4be0ad30dea91cf9893e37602ae3ab47ad5fd5af
AlejandroTene <carlostene2006@gmail.com>
2024-08-15 10:44:03 -0500

    se implemento mas condiciones en promedioNotas y el programa funciona todo OK

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,9 +5,18 @@ calcularPromedioNotas=function(){
     let promedio=calcularPromedio(n1Float,n2Float,n3Float);
     let promedioRedondeado=promedio.toFixed(2);
     cambiarTexto("lblpromedio",promedioRedondeado);
-    if(promedioRedondeado>7){
+
+    if(promedioRedondeado<5 && promedioRedondeado>0){
+        cambiarTexto("lblinfo","REPROBADO");
+        cambiarImagen("imgV","reprobado.gif");
+    }else if(promedioRedondeado>=5 && promedioRedondeado<=8){
+        cambiarTexto("lblinfo","BUEN TRABAJO");
+        cambiarImagen("imgV","buen.gif");
+    }else if(promedioRedondeado>8 && promedioRedondeado<=10){
+        cambiarTexto("lblinfo","EXCELENTE");
         cambiarImagen("imgV","exito.gif");
     }else{
-        cambiarImagen("imgV","fracaso.gif");
+        cambiarTexto("lblinfo","DATOS INCORRECTOS");
+        cambiarImagen("imgV","incorrecto.gif");
     }
 }
