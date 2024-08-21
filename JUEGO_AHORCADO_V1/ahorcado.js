68e3f5f4a4c6bab9508154433ed3791fe93a4137
AlejandroTene <carlostene2006@gmail.com>
2024-08-21 13:22:14 -0500

    paso 6 completado todo OK

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -60,6 +60,7 @@ validar = function (letra) {
     if(letrasEncontradas==0){
         alert("La letra no es parte de la palabra");
         errores++;
+        mostrarAhorcado();
     }
 }
 
@@ -78,6 +79,35 @@ ingresarLetra = function () {
     }else{
         alert("Solo se aceptan mayúsculas");
     }
+}
+
+mostrarAhorcado= function () {
+    if(errores==1){
+        mostrarImagen("ahorcadoImagen","Ahorcado_01.png");
+    }else if(errores==2){
+        mostrarImagen("ahorcadoImagen","Ahorcado_02.png");
+    }else if(errores==3){
+        mostrarImagen("ahorcadoImagen","Ahorcado_03.png");
+    }else if(errores==4){
+        mostrarImagen("ahorcadoImagen","Ahorcado_04.png");
+    }else if(errores==5){
+        mostrarImagen("ahorcadoImagen","Ahorcado_05.png");
+    }else if(errores==6){
+        mostrarImagen("ahorcadoImagen","Ahorcado_06.png");
+    }else if(errores==7){
+        mostrarImagen("ahorcadoImagen","Ahorcado_07.png");
+    }else if(errores==8){
+        mostrarImagen("ahorcadoImagen","Ahorcado_08.png");
+    }else if(errores==9){
+        mostrarImagen("ahorcadoImagen","Ahorcado_09.png");
+    }
+    
+}
+
+mostrarImagen = function (idComponente, rutaImagen) {
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.src = rutaImagen;
 }
 
 
